943579e7606a513761f4a6331ed79c291b50e8e4
BakytDzhumabev <djumabaevb@gmail.com>
2021-05-23 21:07:44 +0600

    Initial Commit

---
+++ SegueSwiftApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SegueSwiftApp
+//
+//  Created by Bakyt Dzhumabaev on 23/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
