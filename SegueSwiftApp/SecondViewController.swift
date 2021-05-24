12d345cc1de279477af3de5d97e9997ddc76649b
BakytDzhumabev <djumabaevb@gmail.com>
2021-05-24 19:54:22 +0600

    add segue to button

---
+++ SegueSwiftApp/SecondViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SecondViewController.swift
+//  SegueSwiftApp
+//
+//  Created by Bakyt Dzhumabaev on 23/5/21.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var secondViewLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
