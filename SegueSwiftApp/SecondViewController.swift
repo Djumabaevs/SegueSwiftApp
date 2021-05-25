b5d07cb9dae718fec4e02d4a7bcfc0cec3bacd9c
BakytDzhumabev <djumabaevb@gmail.com>
2021-05-25 21:05:23 +0600

    transfer from 1 view to 2nd

--- SegueSwiftApp/SecondViewController.swift
+++ SegueSwiftApp/SecondViewController.swift
@@ -10,13 +10,13 @@ import UIKit
 class SecondViewController: UIViewController {
 
     @IBOutlet weak var secondViewLabel: UILabel!
-    
     @IBOutlet weak var nameLabel: UILabel!
+    var myName = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        nameLabel.text = myName
+        
     }
     
 
--- SegueSwiftApp/ViewController.swift
+++ SegueSwiftApp/ViewController.swift
@@ -10,16 +10,25 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var firstViewlabel: UILabel!
-    
     @IBOutlet weak var nameText: UITextField!
+    var userName = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
     @IBAction func nextClicked(_ sender: Any) {
+        userName = nameText.text!
+        performSegue(withIdentifier: "gotoSecondVC", sender: nil)
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "gotoSecondVC" {
+            let destinationVC = segue.destination as! SecondViewController
+            destinationVC.myName = userName
+        }
+    } 
     
 }
 
